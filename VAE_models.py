c0fd1655185eaad00eddada42432b76ade1583de
Elias Brand <115883655+Elias-desig@users.noreply.github.com>
2025-06-14 21:04:58 +0200

    Update VAE_models.py
    Added forward, relus

--- VAE_models.py
+++ VAE_models.py
@@ -7,3 +7,8 @@ class Encoder(nn.module):
     super().__init__()
     self.layer_dims = layer_dimensions(in_dim, latent_dim, num_layers)
     self.linears = nn.ModuleList([nn.Linear(self.layer_dims[i]) for i in range(num_layers)])
+    self.relus   = nn.ModuleList([nn.ReLU() for _ in range(num_layers)])
+  def forward(self. x):
+    for i, layer in enumerate(self.linears):
+      x = self.relus[i](layer(x))
+    return x

b9c8576bbf280b7a7184ad394825abbeee55ffb8
Elias Brand <115883655+Elias-desig@users.noreply.github.com>
2025-06-15 15:54:38 +0200

    Update VAE_models.py
    Decoder

--- VAE_models.py
+++ VAE_models.py
@@ -12,3 +12,15 @@ class Encoder(nn.module):
     for i, layer in enumerate(self.linears):
       x = self.relus[i](layer(x))
     return x
+
+class Decoder(nn.module):
+  def __init__(self, in_dim, latent_dim, num_layers):
+    super().__init__()
+    self.layer_dims = layer_dimensions(in_dim, latent_dim, num_layers)
+    self.linears = nn.ModuleList([nn.Linear(self.layer_dims[i]) for i in range(num_layers)])
+    self.relus   = nn.ModuleList([nn.ReLU() for _ in range(num_layers-1)])
+  def forward(self. x):
+    for i, layer in enumerate(self.linears):
+      x = self.relus[i](layer(x))
+    x = nn.sigmoid(x)
+    return x

b91b8517df6f3b90ffa02bcfa87852a7cee61341
koraygecimli <kgecimli@uni-osnabrueck.de>
2025-06-23 11:50:52 +0200

    NF class ersten Funktionen

--- nf_model.py
+++ nf_model.py
@@ -1,30 +1,29 @@
-import numpy as np
-import matplotlib.pyplot as plt
+class NormaFlows(nn.Module, ):
+    def __init__(self, input):
+        super().__init__()  # <--- hier richtig
+        self.weights = np.random.random(len(input))
+        self.bias = np.random.random(len(input))
+        self.values = input
 
-# 1. Input-Verteilung: 2D-Gitter
-x = np.linspace(-4, 4, 100)
-y = np.linspace(-4, 4, 100)
-u1, u2 = np.meshgrid(x, y)
-u = np.stack([u1.ravel(), u2.ravel()], axis=1)
+    def lin_flows(self):
+        self.z = np.matmul(self.values, self.weights) + self.bias
+        return self.z
 
-# 2. Definiere s(u1) und t(u1) — wie ein kleines Netz
-def s(u1): return 0.5 * np.tanh(u1)
-def t(u1): return 1.0 * np.sin(u1)
+    def norm_prob(self, mean=0, std_dev=1):
+        coeff = 1 / (std_dev * np.sqrt(2 * np.pi))
+        exponent = -0.5 * ((self.z - mean) / std_dev) ** 2
+        self.px = coeff * np.exp(exponent)
+        return self.px
 
-# 3. Affine Coupling Transformation
-v1 = u[:, 0]
-v2 = u[:, 1] * np.exp(s(u[:, 0])) + t(u[:, 0])
-v = np.stack([v1, v2], axis=1)
+    def change_of_variable(self):
+        x = (self.values - self.bias) / self.weights
+        jacobian = np.abs(1 / np.prod(
+            self.weights))  # to do funktion für jacobian schreiben, da das nur für vektoren bsisher funktioner
+        self.py = self.px * jacobian  # Fehler: gibt noch zwei werte aus, muss mathemathisch angepasst werden
 
-# Dein Plot (vereinfachtes Beispiel)
-fig, ax = plt.subplots()
-ax.plot([0, 1], [0, 1])
+        return self.py
 
-# Zeichne den Canvas
-fig.canvas.draw()
 
-# Bilddaten als RGBA-Array holen
-img = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8)
-img = img.reshape(fig.canvas.get_width_height()[::-1] + (4,))
 
-print(img.shape)  # z.B. (480, 640, 4)
+
+

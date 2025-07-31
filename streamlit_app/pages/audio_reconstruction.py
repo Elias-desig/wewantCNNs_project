0ba90da60367fb49e75e03376eef4dc17efb59cb
Elias-desig <eliasbrand@icloud.com>
2025-07-31 02:04:09 +0200

    Option menus, core as package

--- streamlit_app/pages/audio_reconstruction.py
+++ streamlit_app/pages/audio_reconstruction.py
@@ -1,5 +1,11 @@
 import streamlit as st
+from core import load_model, reconstruction
 
 
+model_options = st.selectbox("Select a Model", ["VAE", "Convolutional VAE", "Autoregressive NF"])
+st.write(model_options)
+instrument_source = st.selectbox('Instrument Source', ['acoustic', 'electronic', 'synthetic'])
+instrument_family = st.selectbox('Instrument Familty', ['bass', 'brass', 'flute', 'guitar', 'keyboard', 'mallet', 'organ', 'reed', 'string', 'synth_lead', 'vocal'])
+note_qualities = st.multiselect('Note Qualities', ['bright', 'dark', 'distortion', 'fast_decay', 'long_release', 'multiphonic', 'nonlinear_env', 'percussive', 'reverb', 'tempo-synced'])
 st.write('Synthesize audio with Normalizing Flow:')
 st.write('Synthesize audio with VAE:')
